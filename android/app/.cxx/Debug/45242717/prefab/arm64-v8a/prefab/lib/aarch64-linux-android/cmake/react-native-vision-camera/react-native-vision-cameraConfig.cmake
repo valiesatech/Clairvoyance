fc39b4072f8b13c591cc3b190d57fa015ae1e09f
Developer <developer@example.com>
2026-03-15 14:26:28 -0500

    feat: final submission - Claire AI voice assistant

---
+++ android/app/.cxx/Debug/45242717/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-vision-camera/react-native-vision-cameraConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-vision-camera::VisionCamera)
+add_library(react-native-vision-camera::VisionCamera SHARED IMPORTED)
+set_target_properties(react-native-vision-camera::VisionCamera PROPERTIES
+    IMPORTED_LOCATION "C:/Users/Valie/Hackathon/Clairvoyance/node_modules/react-native-vision-camera/android/build/intermediates/cxx/Debug/a1jx1c24/obj/arm64-v8a/libVisionCamera.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Valie/Hackathon/Clairvoyance/node_modules/react-native-vision-camera/android/build/headers/visioncamera"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
